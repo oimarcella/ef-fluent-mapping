2db14f5a4c1bfd0f526150c6195b05b5f6845313
Marcella Amorim <devabefarma4@outlook.com>
2024-06-21 20:08:46 -0300

    add: inserindo objetos que possuem outros objetos dentro de si

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
 
 namespace Blog;
 
@@ -7,5 +10,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Vamos estudar EF e Fluent Mapping!");
+
+        using var ctx = new BlogDataContext();
+        /*ctx.Users.Add(new Models.User{
+            Name = "Fulano de Tal",
+            Email = "fulano@tal.com.br",
+            Image = "muimg.png",
+            Bio = "Muito falado por aí",
+            Slug = "otalfulano",
+            PasswordHash = "123abc"
+        });*/
+
+        var user = ctx.Users.FirstOrDefault(x=> x.Id == 1011);
+        ctx.Posts.Add(new Post {
+            Title = "Como começar no ecossistema .NET",
+            Body = "Meu artigo",
+            Category = new Category {
+                Name = "UI",
+                Slug = "user-interface"
+            },
+            Author = user,
+            CreateDate = DateTime.Now,
+            Slug = "comecando-no-dotnet",
+            Summary = "Ola dev",
+            //Tags = null
+        });
+
+        ctx.SaveChanges();
     }
 }
